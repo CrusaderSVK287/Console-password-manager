5fe04faf778c8f32056e86e063af0122cc4cb8ab
Lukáš Belán <lukasbelan@student.tuke.sk>
2022-08-22 15:55:51 +0200

    Fix: Fixed various bugs regarding security level

--- src/password.c
+++ src/password.c
@@ -9,13 +9,45 @@ char* get_password() {
 
 bool is_password_valid(char* password) {
     int len = strlen(password);
+    bool has_uppercase = false;
+    bool has_lowercase = false;
+    bool has_number = false;
+    bool has_special_char = false;
 
+    //Password too short is considered weak by default
     if(len < 9) {
         return print_password_security_level(PASSL_WEAK);
     }
 
+    for (size_t i=0; i<len; i++) {
+        switch (password[i])
+        {
+        case 'a'...'z': has_lowercase = true;
+            break;
+        case 'A'...'Z': has_uppercase = true;
+            break;
+        case '0'...'9': has_number = true;
+            break;
+        case '!':   //fallthrough
+        case '@':   //fallthrough
+        case '^':   //fallthrough
+        case '*':   //fallthrough
+        case '#'...'&' : has_special_char = true;   // #...& contains # $ % &
+            break;
+        case '\n': break;   //to not evaluate as invalid char
+        default:
+            fprintf(stderr,"%sWarning: '%c' is not a valid character, please choose another password%s\n",STRC_YELLOW,password[i],STRC_DEFAULT);
+            return false;
+            break;
+        }
+    }
 
-    return true;
+    int security_level = 0;
+    if(has_lowercase && has_uppercase) security_level++;
+    if(has_number) security_level++;
+    if(has_special_char) security_level++;
+
+    return print_password_security_level(security_level);
 }
 
 char* hash256(char* password) {
@@ -33,16 +65,18 @@ bool print_password_security_level(int level) {
         char choice;
         scanf("%c",&choice);
         fflush(stdin);
-        return choice == 'y' || choice == 'Y';
+        return (choice == 'y' || choice == 'Y');
+        printf("WHY THE FUCK???");
+    case 0:
     case PASSL_WEAK:
         printf("%sThis password is weak. Password must be at least 8 characters long and contain at least two of these:\n\
         - One uppercase and one lowercase letter\n\
         - a number\n\
-        - a special character\n%s",STRC_RED,STRC_DEFAULT);
+        - a special character (!, @, #, $, %%, ^, &, *)\n%s",STRC_RED,STRC_DEFAULT);
         return false;
     default:
         fprintf(stderr,"Invalid password security level\n");
         break;
     }
-    return true;
+    return false;
 }
