2646422f787ea41b8ffe406902f9bdebb64aae37
Lukáš Belán <lukasbelan@student.tuke.sk>
2022-08-25 13:20:01 +0200

    added help.h header file file contains declarations for the print help functions, this module will be filled gradually

---
+++ src/headers/help.h
@@ -0,0 +1,15 @@
+#include "defines.h"
+
+/**
+ * @brief Function prints all the help pages
+ * 
+ */
+void command_help();
+
+/**
+ * @brief Each of theese functions prints a help page for a coresponding command
+ * 
+ */
+void help_command_help();
+
+void help_command_new();
