22bf92c7e694d6ad98bd1b2dd613cc7bbc583f57
Lukáš Belán <lukasbelan@student.tuke.sk>
2022-08-25 13:31:00 +0200

    help page for help command implemented

--- src/help.c
+++ src/help.c
@@ -9,7 +9,8 @@ void command_help(struct options* opt) {
 }
 
 void help_command_help() {
-    
+    printf("%sCommand:%s help\n",STRC_CYAN,STRC_DEFAULT);
+    printf("\tThis command prints help pages for all commands.\n\tOptitional flags: NONE\n");
 }
 
 void help_command_new() {
